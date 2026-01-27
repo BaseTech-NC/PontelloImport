d75c063d4686c94ffa132573e1d39ec9febf1bf6
Solomonwole <solomonwolemail@gmail.com>
2026-01-27 15:48:52 -0500

    Add project files.

---
+++ PontelloImport/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace PontelloImport.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string SKU { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+
+
+    }
+}
