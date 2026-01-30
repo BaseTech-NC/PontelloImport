78c5295543eaf792dc06a5fd824803484567b4e5
Solomonwole <solomonwolemail@gmail.com>
2026-01-29 21:15:44 -0500

    Fixed scaffold controller not generating issue, I updated the packages versions

---
+++ PontelloImport/Data/PontelloDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PontelloImport.Models;
+
+namespace PontelloImport.Data
+{
+    public class PontelloDbContext : DbContext
+    {
+        public PontelloDbContext(DbContextOptions<PontelloDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Product> Product { get; set; }
+    }
+  
+}
